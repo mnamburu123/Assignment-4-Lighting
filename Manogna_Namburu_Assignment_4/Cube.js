9966d38fd275fe4c1cb5f081235928bc3d7f9a87
mnamburu123 <102500142+mnamburu123@users.noreply.github.com>
2024-05-26 18:04:27 -0700

    Add files via upload

---
+++ Manogna_Namburu_Assignment_4/Cube.js
@@ -0,0 +1,61 @@
+class Cube{
+    constructor(){
+      this.type = 'cube';
+      this.color = [1.0, 1.0, 1.0, 1.0];
+      this.matrix = new Matrix4();
+      this.textureNum = -1; 
+    }
+    render() {
+      
+      var rgba = this.color;
+      
+
+      // pass the texture number
+      gl.uniform1i(u_whichTexture, this.textureNum);
+
+      // Pass the color of a point to u_FragColor variable
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+      // Pass the matrix of a point to u_ModelMatrix variable
+      gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+      
+      // front of cube 
+      drawTriangle3DUVNormal( [0,0,0 , 1,1,0, 1,0,0 ], [0,0, 1,1, 1,0], [0,0,-1, 0,0,-1, 0,0,-1]);
+      drawTriangle3DUVNormal( [0,0,0 , 0,1,0, 1,1,0 ], [0,0, 0,1, 1,1], [0,0,-1, 0,0,-1, 0,0,-1]);
+
+      // Pass the color of a point to u_FragColor variable
+      //gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+      // top
+      drawTriangle3DUVNormal([0,1,0, 0,1,1, 1,1,1], [0,0, 0,1, 1,1], [0,1,0, 0,1,0, 0,1,0]);
+      drawTriangle3DUVNormal([0,1,0, 1,1,1, 1,1,0], [0,0, 1,1, 1,0], [0,1,0, 0,1,0, 0,1,0]);
+
+      // Pass the color of a point to u_FragColor variable
+      //gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
+
+      // right
+      //gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
+      drawTriangle3DUVNormal([1,1,0, 1,1,1, 1,0,0], [0,0, 0,1, 1,1], [1,0,0, 1,0,0, 1,0,0]);
+      drawTriangle3DUVNormal([1,0,0, 1,1,1, 1,0,1], [0,0, 1,1, 1,0], [1,0,0, 1,0,0, 1,0,0]);
+
+      // left 
+      //gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
+      drawTriangle3DUVNormal([0,1,0, 0,1,1, 0,0,0], [0,0, 0,1, 1,1], [-1,0,0, -1,0,0, -1,0,0]);
+      drawTriangle3DUVNormal([0,0,0, 0,1,1, 0,0,1], [0,0, 1,1, 1,0], [-1,0,0, -1,0,0, -1,0,0]);
+
+      // bottom
+      //gl.uniform4f(u_FragColor, rgba[0]*.6, rgba[1]*.6, rgba[2]*.6, rgba[3]);
+      drawTriangle3DUVNormal([0,0,0, 0,0,1, 1,0,1], [0,0, 0,1, 1,1], [0,-1,0, 0,-1,0, 0,-1,0]);
+      drawTriangle3DUVNormal([0,0,0, 1,0,1, 1,0,0], [0,0, 1,1, 1,0], [0,-1,0, 0,-1,0, 0,-1,0]);
+
+      // back 
+      //gl.uniform4f(u_FragColor, rgba[0]*.5, rgba[1]*.5, rgba[2]*.5, rgba[3]);
+      drawTriangle3DUVNormal([0,0,1, 1,1,1, 1,0,1], [0,0, 0,1, 1,1], [0,0,1, 0,0,1, 0,0,1]);
+      drawTriangle3DUVNormal([0,0,1, 0,1,1, 1,1,1], [0,0, 1,1, 1,0], [0,0,1, 0,0,1, 0,0,1]);
+
+
+  }
+  }
+  
+  
